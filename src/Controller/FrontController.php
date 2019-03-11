0d9382b91cd89081bf287b7c860f9f83eaf39012
Bento <oobento01oo@gmail.com>
2019-03-11 22:02:37 +0100

    Ajout Footer, Navbar, créations des templates

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,23 +3,60 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FrontController extends AbstractController
 {
+
     /**
-     * Page Accueil
+     * Page Formations
      */
-    public function index()
+    public function formations(): Response
     {
-        return $this->render('front/index.html.twig');
+        return $this->render('front/formations.html.twig');
+    }
+
+    /**
+     * Page Filiere
+     */
+    public function filiere(): Response
+    {
+        return $this->render('front/filiere.html.twig');
+    }
+
+    /**
+     * Page Dispositifs Spéciaux : ULIS => Unité Localisé pour l'Inclusion Scolaire
+     */
+    public function ulis(): Response
+    {
+        return $this->render('front/ulis.html.twig');
+    }
+
+    /**
+     * Page Dispositifs Spéciaux : MLDS => Mission de Lutte contre le Décrochage Scolaire
+     */
+    public function mlds(): Response
+    {
+        return $this->render('front/mlds.html.twig');
+    }
+
+    /**
+     * Page Questionnaires
+     */
+    public function questionnaires(): Response
+    {
+        return $this->render('front/questionnaires.html.twig');
     }
 
     /**
      * Page Histoire du Lycée
      */
-    public function histoire()
+    public function histoire(): Response
     {
         return $this->render('front/histoire.html.twig');
     }
+
+
 
 }
