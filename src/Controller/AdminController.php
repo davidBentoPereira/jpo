4aef5e802731217dbd9f612cee5fe89eb174cca5
Celine <celine.duclos01@gmail.com>
2019-03-18 11:55:45 +0100

    level up les events

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
@@ -18,11 +21,21 @@ class AdminController extends AbstractController
 
     public function event()
     {
-        return $this->render('admin/event.html.twig');
+        $repository = $this->getDoctrine()->getRepository(Event::class);;
+        $events = $repository->findAll();
+        return $this->render('admin/event.html.twig',
+            ["events" => $events]);
     }
 
     public function survey()
     {
         return $this->render('admin/survey.html.twig');
+    }
+
+    public function addEvent()
+    {
+        $form = $this->createForm(EventType::class);
+        return $this->render('admin/addEvent.html.twig',
+            ['form' => $form->createView()]);
     }
 }
