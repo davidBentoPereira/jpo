3e8d318f91ec45fe442c1006590dcc30e497fe3e
Celine <celine.duclos01@gmail.com>
2019-03-21 15:10:48 +0100

    Ajout et modification d'une question d'un sondage

--- src/Controller/admin/SurveyController.php
+++ src/Controller/admin/SurveyController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Question;
 use App\Entity\Survey;
+use App\Form\QuestionType;
 use App\Form\SurveyType;
 use App\Repository\EventRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\QuestionTypeRepository;
 use App\Repository\SurveyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -88,5 +92,75 @@ class SurveyController extends AbstractController
 
         return $this->render('admin/resultSurvey.html.twig',
             ['survey' => $survey]);
+    }
+
+    /*---- QUESTIONS -----*/
+
+    public function addQuestion($id, Request $request, QuestionRepository $questionRepository, SurveyRepository $surveyRepository, QuestionTypeRepository $questionTypeRepository)
+    {
+        $survey = $surveyRepository->findOneBy(['id' => $id]);
+        $form = $this->createForm(QuestionType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if($request->getMethod() == 'POST')
+            {
+                $entityManager = $this->getDoctrine()->getManager();
+                $data = $form->getData();
+                $title = $data['title'];
+                $idQuestionType = $data['questionType'];
+                $question = new Question($title);
+                $question->setQuestionType($questionTypeRepository->findOneBy(['id' => $idQuestionType]));
+                $question->setSurvey($survey);
+                $entityManager->persist($question);
+                $entityManager->flush();
+            }
+            return $this->redirectToRoute('editSurvey', ['id'=>$id]);
+        }
+
+        return $this->render('admin/formQuestion.html.twig',
+            ['form' => $form->createView(),
+                'type' => 'add',
+                'survey' => $survey]);
+    }
+
+    public function editQuestion($idSurvey, $idQuestion, Request $request, QuestionRepository $questionRepository, SurveyRepository $surveyRepository, QuestionTypeRepository $questionTypeRepository)
+    {
+        $survey = $surveyRepository->findOneBy(['id' => $idSurvey]);
+        $question = $questionRepository->findOneBy(['id' => $idQuestion]);
+        $form = $this->createForm(QuestionType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if($request->getMethod() == 'POST')
+            {
+                $entityManager = $this->getDoctrine()->getManager();
+                $data = $form->getData();
+                $title = $data['title'];
+                $idQuestionType = $data['questionType'];
+                $question->setTitle($title);
+                $question->setQuestionType($questionTypeRepository->findOneBy(['id' => $idQuestionType]));
+                $question->setSurvey($survey);
+                $entityManager->persist($question);
+                $entityManager->flush();
+            }
+            return $this->redirectToRoute('editSurvey', ['id'=>$idSurvey]);
+        }
+
+        return $this->render('admin/formQuestion.html.twig',
+            ['form' => $form->createView(),
+                'type' => 'edit',
+                'question' => $question,
+                'survey' => $survey]);
+    }
+
+    public function deleteQuestion($id, $idQuestion, QuestionRepository $questionRepository, SurveyRepository $surveyRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $survey = $surveyRepository->findOneBy(['id' => $id]);
+        $question = $questionRepository->findOneBy(['id' => $idQuestion]);
+        $entityManager->remove($question);
+        $entityManager->flush();
+        return $this->redirectToRoute('editSurvey', ['id'=>$id]);
     }
 }
