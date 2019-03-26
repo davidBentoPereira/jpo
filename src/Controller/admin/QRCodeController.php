23d86d7b71a1462ef423d066106f18bcfc733e24
Yannick Debrée <debree.yannick13@gmail.com>
2019-03-26 02:09:38 +0100

    initialisation de docker-compose et MVP de la création de qrcodes

--- src/Controller/admin/QRCodeController.php
+++ src/Controller/admin/QRCodeController.php
@@ -6,6 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\FiliereRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\LabelAlignment;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 class QRCodeController extends AbstractController
 {
@@ -17,12 +23,23 @@ class QRCodeController extends AbstractController
     public function rendering(Request $request)
     {
         $protocol = $request->isSecure() ? "https" : "http";
-        $fullUrl =
-            $protocol
-            ."://"
-            .$request->getHost()
-            .":".$request->getPort()
-            .$request->query->get('url');
+        $port = $request->getPort() != 8000 ? "" : ":".$request->getPort();
+        $domain = $protocol."://".$request->getHost().$port;
+
+        $fullUrl = $domain.$request->query->get('url');
+
+        $qrCode = new QrCode($fullUrl);
+        $qrCode->setSize(300);
+        $qrCode->setMargin(10);
+        $qrCode->setEncoding('UTF-8');
+
+        return new QrCodeResponse($qrCode);
+
+        // $fileSystem = new Filesystem();
+        // $fileSystem->touch($domain.'/img/qrcode/test.png');
+        // $fileSystem
+
+        // $qrCode->writeFile($domain.'/img/qrcode/test.png');
 
         return $this->render('admin/qrcodes/rendering.html.twig', [
             'url' => $fullUrl
