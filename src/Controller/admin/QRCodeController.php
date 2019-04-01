d1ea1b698889aec143579c01216136d65c2126cd
Yannick Debrée <debree.yannick13@gmail.com>
2019-04-01 12:14:21 +0200

    Finition de la génération des pdf

--- src/Controller/admin/QRCodeController.php
+++ src/Controller/admin/QRCodeController.php
@@ -12,6 +12,7 @@ use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Response\QrCodeResponse;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use App\Service\SlugGeneratorService;
 
 class QRCodeController extends AbstractController
 {
@@ -23,27 +24,34 @@ class QRCodeController extends AbstractController
     public function rendering(Request $request)
     {
         $protocol = $request->isSecure() ? "https" : "http";
+
         $port = $request->getPort() == 80 ? "" : ":".$request->getPort();
+
         $domain = $protocol."://".$request->getHost().$port;
 
-        $fullUrl = $domain.$request->query->get('url');
+        $pageUrl = $domain.$request->query->get('url');
 
-        $qrCode = new QrCode($fullUrl);
+        $qrCode = new QrCode($pageUrl);
+
+        $fileType = 'png';
+
+        $qrCode->setWriterByName($fileType);
+
         $qrCode->setSize(300);
+
         $qrCode->setMargin(10);
+
         $qrCode->setEncoding('UTF-8');
 
-        // return new QrCodeResponse($qrCode);
+        $fileName = new SlugGeneratorService($request->query->get('slug'));
 
-        // $fileSystem = new Filesystem();
-        // $fileSystem->touch($domain.'/img/qrcode/test.png');
-        // $fileSystem
+        $filePath = '/img/qrcode/'.$fileName->getSlug().'.'.$fileType;
 
-        // $qrCode->writeFile($domain.'/img/qrcode/test.png');
+        $qrCode->writeFile(__DIR__.'/../../../public'.$filePath);
 
         return $this->render('admin/qrcodes/rendering.html.twig', [
-            'url' => $fullUrl,
-            'qrCode' => $qrCode
+            'pageUrl' => $pageUrl,
+            'fileUrl' => $domain.$filePath
         ]);
     }
 }
